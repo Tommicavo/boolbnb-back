24accf38f61c1e993f04790bd8740b7dda82d35c
Skyfe24 <fabio.erase@gmail.com>
2023-09-25 16:54:11 +0200

    CategorySeeder completed

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,16 +2,37 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Generator $faker): void
     {
-        //
+        $labels = [
+            'Villa',
+            'Appartamento',
+            'Chalet',
+            'Cascina',
+        ];
+
+
+
+
+
+
+        foreach ($labels as $label) {
+            $category = new Category;
+
+            $category->label = $label;
+            $category->color = $faker->hexColor();
+            $category->save();
+        };
     }
 }
