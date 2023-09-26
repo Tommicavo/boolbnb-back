83054c63f0fa7897450d9d737463f61bf1a528a0
marcocareddu <saldumaccu@tuta.io>
2023-09-26 14:59:24 +0200

    Added description validation in estate controller

--- app/Http/Controllers/admin/EstateController.php
+++ app/Http/Controllers/admin/EstateController.php
@@ -36,9 +36,9 @@ class EstateController extends Controller
         // Validation
         $request->validate(
             [
-                'title' => 'required|string',
-                'description' => 'nullable|string',
-                'cover' => 'nullable',
+                'title' => 'required|string|max:50',
+                'description' => 'nullable|string|max:300',
+                'cover' => 'nullable|image',
                 'rooms' => 'required|numeric:1,254',
                 'beds' => 'required|numeric:1,254',
                 'bathrooms' => 'required|numeric:1,254',
@@ -47,6 +47,8 @@ class EstateController extends Controller
             ],
             [
                 'title.required' => 'Il titole è obbligatorio.',
+                'title.max' => 'Il titole deve essere lungo max 50 carattari.',
+                'description.max' => 'La descrizione deve essere lunga max 300 carattari.',
                 'rooms.required' => 'Il numero delle stanze è obbligatorio.',
                 'rooms.required' => 'Il numero delle stanze deve essere compreso tra 1 e 254.',
                 'beds.required' => 'Il numero dei letti è obbligatorio.',
@@ -54,9 +56,10 @@ class EstateController extends Controller
                 'bathrooms.required' => 'Il numero dei bagni è obbligatorio.',
                 'bathrooms.required' => 'Il numero dei bagni deve essere compreso tra 1 e 254.',
                 'mq.required' => 'Il numero dei mq è obbligatorio.',
-                'mq.required' => 'Il mq devono essere compresi tra 20 e 1000.',
+                'mq.required' => 'I mq devono essere compresi tra 20 e 1000.',
                 'price.required' => 'Il prezzo è obbligatorio.',
-                'price.required' => 'Il prezzo deve essere compreso tra 0.01 e 254.00.'
+                'price.required' => 'Il prezzo deve essere compreso tra € 0.01 ed € 254.00.',
+                'cover.image' => "È possibile allegare solo file di tipo immagine."
             ]
         );
 
@@ -100,9 +103,9 @@ class EstateController extends Controller
         // Validation
         $request->validate(
             [
-                'title' => 'required|string',
-                'description' => 'nullable|string',
-                'cover' => 'nullable',
+                'title' => 'required|string|max:50',
+                'description' => 'nullable|string|max:300',
+                'cover' => 'nullable|image',
                 'rooms' => 'required|numeric:1,254',
                 'beds' => 'required|numeric:1,254',
                 'bathrooms' => 'required|numeric:1,254',
@@ -111,6 +114,8 @@ class EstateController extends Controller
             ],
             [
                 'title.required' => 'Il titole è obbligatorio.',
+                'title.max' => 'Il titole deve essere lungo max 50 carattari.',
+                'description.max' => 'La descrizione deve essere lunga max 300 carattari.',
                 'rooms.required' => 'Il numero delle stanze è obbligatorio.',
                 'rooms.required' => 'Il numero delle stanze deve essere compreso tra 1 e 254.',
                 'beds.required' => 'Il numero dei letti è obbligatorio.',
@@ -118,9 +123,10 @@ class EstateController extends Controller
                 'bathrooms.required' => 'Il numero dei bagni è obbligatorio.',
                 'bathrooms.required' => 'Il numero dei bagni deve essere compreso tra 1 e 254.',
                 'mq.required' => 'Il numero dei mq è obbligatorio.',
-                'mq.required' => 'Il mq devono essere compresi tra 20 e 1000.',
+                'mq.required' => 'I mq devono essere compresi tra 20 e 1000.',
                 'price.required' => 'Il prezzo è obbligatorio.',
-                'price.required' => 'Il prezzo deve essere compreso tra 0.01 e 254.00.'
+                'price.required' => 'Il prezzo deve essere compreso tra € 0.01 ed € 254.00.',
+                'cover.image' => "È possibile allegare solo file di tipo immagine."
             ]
         );
 
