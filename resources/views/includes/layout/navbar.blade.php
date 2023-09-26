aa76613b790fc439c08d5f67c44db489bc982f3a
marcocareddu <saldumaccu@tuta.io>
2023-09-26 10:55:46 +0200

    Fixed navbar

--- resources/views/includes/layout/navbar.blade.php
+++ resources/views/includes/layout/navbar.blade.php
@@ -8,6 +8,18 @@
                         <a class="nav-link @if (request()->routeIs('admin.home')) active @endif"
                             href="{{ route('admin.home') }}">Home</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link @if (request()->routeIs('admin.estates.index')) active @endif"
+                            href="{{ route('admin.estates.index') }}">Index</a>
+                    </li>
+                    {{-- <li class="nav-item">
+                        <a class="nav-link @if (request()->routeIs('admin.estates.trash')) active @endif"
+                            href="{{ route('admin.estates.trash') }}">Trash</a>
+                    </li> --}}
+                    <li class="nav-item">
+                        <a class="nav-link @if (request()->routeIs('admin.estates.create')) active @endif"
+                            href="{{ route('admin.estates.create') }}">Create</a>
+                    </li>
                 @else
                     <li class="nav-item">
                         <a class="nav-link @if (request()->routeIs('guest.home')) active @endif"
