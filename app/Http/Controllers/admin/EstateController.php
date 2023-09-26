7a7dc2c0da21e82da353cc9dae787a312e5a66a2
marcocareddu <saldumaccu@tuta.io>
2023-09-26 14:44:08 +0200

    Added validation to Estate store/update

--- app/Http/Controllers/admin/EstateController.php
+++ app/Http/Controllers/admin/EstateController.php
@@ -33,9 +33,36 @@ class EstateController extends Controller
      */
     public function store(Request $request)
     {
+        // Validation
+        $request->validate(
+            [
+                'title' => 'required|string',
+                'description' => 'nullable|string',
+                'cover' => 'nullable',
+                'rooms' => 'required|numeric:1,254',
+                'beds' => 'required|numeric:1,254',
+                'bathrooms' => 'required|numeric:1,254',
+                'mq' => 'required|numeric:20,1000',
+                'price' => 'required|required|numeric:0.01'
+            ],
+            [
+                'title.required' => 'Il titole è obbligatorio.',
+                'rooms.required' => 'Il numero delle stanze è obbligatorio.',
+                'rooms.required' => 'Il numero delle stanze deve essere compreso tra 1 e 254.',
+                'beds.required' => 'Il numero dei letti è obbligatorio.',
+                'beds.required' => 'Il numero dei letti deve essere compreso tra 1 e 254.',
+                'bathrooms.required' => 'Il numero dei bagni è obbligatorio.',
+                'bathrooms.required' => 'Il numero dei bagni deve essere compreso tra 1 e 254.',
+                'mq.required' => 'Il numero dei mq è obbligatorio.',
+                'mq.required' => 'Il mq devono essere compresi tra 20 e 1000.',
+                'price.required' => 'Il prezzo è obbligatorio.',
+                'price.required' => 'Il prezzo deve essere compreso tra 1 e 254.',
+            ]
+        );
+
         $data = $request->all();
 
-        // Change is_visible switch value to boolean one.
+        // Change is_visible switch value to a boolean one.
         $data['is_visible'] = isset($data['is_visible']);
 
         $estate = new Estate;
@@ -70,6 +97,33 @@ class EstateController extends Controller
      */
     public function update(Request $request, Estate $estate)
     {
+        // Validation
+        $request->validate(
+            [
+                'title' => 'required|string',
+                'description' => 'nullable|string',
+                'cover' => 'nullable',
+                'rooms' => 'required|numeric:1,254',
+                'beds' => 'required|numeric:1,254',
+                'bathrooms' => 'required|numeric:1,254',
+                'mq' => 'required|numeric:20,1000',
+                'price' => 'required|required|numeric:0.01'
+            ],
+            [
+                'title.required' => 'Il titole è obbligatorio.',
+                'rooms.required' => 'Il numero delle stanze è obbligatorio.',
+                'rooms.required' => 'Il numero delle stanze deve essere compreso tra 1 e 254.',
+                'beds.required' => 'Il numero dei letti è obbligatorio.',
+                'beds.required' => 'Il numero dei letti deve essere compreso tra 1 e 254.',
+                'bathrooms.required' => 'Il numero dei bagni è obbligatorio.',
+                'bathrooms.required' => 'Il numero dei bagni deve essere compreso tra 1 e 254.',
+                'mq.required' => 'Il numero dei mq è obbligatorio.',
+                'mq.required' => 'Il mq devono essere compresi tra 20 e 1000.',
+                'price.required' => 'Il prezzo è obbligatorio.',
+                'price.required' => 'Il prezzo deve essere compreso tra 1 e 254.',
+            ]
+        );
+
         $data = $request->all();
 
         // Change is_visible switch value to boolean one.
