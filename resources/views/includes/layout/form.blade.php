be84c1b3f1e1bea21ab2d731f54a7f6001a84664
marcocareddu <saldumaccu@tuta.io>
2023-09-26 10:19:44 +0200

    Temporarily removed old & vars

--- resources/views/includes/layout/form.blade.php
+++ resources/views/includes/layout/form.blade.php
@@ -1,24 +1,28 @@
 <div class="container">
-    <form method="POST" action="{{ route('admin.estates.update', $estate) }}" enctype="multipart/form-data">
+    <form method="POST" action="{{ route('admin.estates.store') }}" enctype="multipart/form-data">
         @csrf
-        @method('PUT')
-
-
 
         {{-- Title --}}
         <div class="mb-3 col-12 text-start">
             <label for="title">Nome</label>
-            <input type="text" id="title" name="title"
-                class="form-control bg-dark @error('title') is-invalid @elseif (old('title')) is-valid @enderror"
-                value="{{ old('title', $estate->title) }}" autofocus required>
+            <input type="text" id="title" name="title" class="form-control" autofocus required>
         </div>
 
         {{-- Description --}}
         <div class="mb-3 col-12 text-start">
             <label class="form-label" for="description">Descrizione</label>
-            <textarea type="text" id="description" name="description" class="form-control bg-dark">{{ old('description', $estate->description) }}</textarea>
+            <textarea type="text" id="description" name="description" class="form-control"></textarea>
         </div>
 
+        {{-- Only numbers selectors --}}
+        <div>
 
+            {{-- Rooms --}}
+            <div class="mb-3 col-12 text-start">
+                <label for="rooms">Nome</label>
+                <input type="number" id="rooms" name="rooms" class="form-control" autofocus required>
+            </div>
+
+        </div>
     </form>
 </div>
