8b566c1fe98dffedc562efdbe301fd2388afef51
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-09-26 15:55:49 +0200

    added button to show

--- resources/views/admin/estates/show.blade.php
+++ resources/views/admin/estates/show.blade.php
@@ -3,10 +3,28 @@
 @section('title', $estate->title)
 
 @section('content')
-    <h1 class="mt-3">Dettagli della casa</h1>
 
-    <div class="card my-3">
-        <img src="{{ $estate->cover }}" class="card-img-top" alt="{{ $estate->title }}">
+    <div class="d-flex justify-content-between align-items-center my-3">
+        <h1>Dettagli della casa</h1>
+        <div class="d-flex justify-content-between">
+            <a href="{{ route('admin.estates.index') }}" class="btn btn-outline-secondary me-5">Torna alla lista</a>
+            <div class="d-flex justify-content-end">
+                <a href="{{ route('admin.estates.edit', $estate) }}" class="btn btn-outline-warning">
+                    <i class="fas fa-pencil"></i> Modifica
+                </a>
+                <form action="{{ route('admin.estates.destroy', $estate) }}" method="POST" class="delete-form ms-2">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-outline-danger">
+                        <i class="fas fa-trash"></i> Elimina
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="card mb-3">
+        <img style="height: 26rem; object-fit: cover;" src="{{ $estate->cover }}" class="card-img-top"
+            alt="{{ $estate->title }}">
         <div class="card-body">
             <h5 class="card-title">{{ $estate->title }}</h5>
             <p class="card-text">{{ $estate->description }}</p>
@@ -17,23 +35,6 @@
             <li class="list-group-item"><strong>Bagni: </strong>{{ $estate->bathrooms }}</li>
             <li class="list-group-item"><strong>Metri Quadri: </strong>{{ $estate->mq }}</li>
             <li class="list-group-item"><strong>Prezzo a notte: </strong>{{ $estate->price }}</li>
-            {{-- linea per le categories quando avremo rotta e controller --}}
-            {{-- linea per gli users quando avremo rotta e controller --}}
         </ul>
     </div>
-    {{-- <footer class="d-flex justify-content-between">
-        <a href="{{ route('admin.estates.index') }}" class="btn btn-outline-secondary">Torna alla lista</a>
-        <div class="d-flex justify-content-end">
-            <a href="{{ route('admin.estates.edit', $post) }}" class="btn btn-outline-warning">
-                <i class="fas fa-pencil"></i> Modifica
-            </a>
-            <form action="{{ route('admin.estates.destroy', $post) }}" method="POST" class="delete-form ms-2">
-                @csrf
-                @method('DELETE')
-                <button class="btn btn-outline-danger">
-                    <i class="fas fa-trash"></i> Elimina
-                </button>
-            </form>
-        </div>
-    </footer> --}}
 @endsection
