8ca9fbbe2dc5add958bf1af12dce534ae08d6d62
marcocareddu <saldumaccu@tuta.io>
2023-09-27 12:30:17 +0200

    Added address inputs

--- resources/views/includes/layout/form.blade.php
+++ resources/views/includes/layout/form.blade.php
@@ -17,16 +17,16 @@
     <h1 class="my-4">
         @if ($estate->exists)
             {{-- Edit section --}}
-            Modifica l'appartamento
+            Modifica l'annuncio
         @else
             {{-- Create section --}}
-            Aggiungi un nuovo appartamento
+            Aggiungi un nuovo annuncio
         @endif
     </h1>
 
     {{-- Title --}}
     <div class="mb-3 col-12">
-        <label for="title">Nome</label>
+        <label for="title">Titolo Annnuncio</label>
         <input type="text" id="title" name="title"
             class="form-control @error('title') is-invalid @elseif (old('title')) is-valid @enderror"
             value="{{ old('title', $estate->title) }}" autofocus required>
@@ -43,11 +43,80 @@
         <textarea type="text" id="description" name="description" class="form-control" maxlength="300">{{ old('description', $estate->description) }}</textarea>
     </div>
 
+    {{-- Address selectors --}}
+    <div class="d-flex row justify-content-between">
+
+        {{-- Toponymic --}}
+        <div class="mb-3 text-start col-2">
+            <label for="toponymic">Tipo</label>
+            <input type="text" id="toponymic" name="toponymic"
+                class="form-control @error('toponymic') is-invalid @elseif (old('toponymic')) is-valid @enderror"
+                value="{{ old('toponymic', $estate->Toponymic) }}" min="1" max="254" required>
+            @error('toponymic')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        {{-- Street name --}}
+        <div class="mb-3 text-start col-3">
+            <label for="street_name">Via</label>
+            <input type="text" id="street_name" name="street_name"
+                class="form-control @error('street_name') is-invalid @elseif (old('street_name')) is-valid @enderror"
+                value="{{ old('street_name', $estate->street_name) }}" min="1" max="254" required>
+            @error('street_name')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        {{-- Number --}}
+        <div class="mb-3 text-start col-1">
+            <label for="number">Civico</label>
+            <input type="number" id="number" name="number"
+                class="form-control @error('number') is-invalid @elseif (old('number')) is-valid @enderror"
+                value="{{ old('number', $estate->number) }}" min="1" max="254" required>
+            @error('number')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        {{-- Zip Code --}}
+        <div class="mb-3 text-start col-1">
+            <label for="zip_code">CAP</label>
+            <input type="number" id="zip_code" name="zip_code"
+                class="form-control @error('zip_code') is-invalid @elseif (old('zip_code')) is-valid @enderror"
+                value="{{ old('zip_code', $estate->zip_code) }}" maxlength="5" minlength="5" required>
+            @error('zip_code')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        {{-- City --}}
+        <div class="mb-3 text-start col-3">
+            <label for="city">Città</label>
+            <input type="text" id="city" name="city"
+                class="form-control @error('city') is-invalid @elseif (old('city')) is-valid @enderror"
+                value="{{ old('city', $estate->city) }}" min="0.01" step="0.01" required>
+            @error('city')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+    </div>
+
     {{-- Only numbers selectors --}}
-    <div class="d-flex flex-column row-cols-6 justify-content-between">
+    <div class="d-flex row justify-content-between">
 
         {{-- Rooms --}}
-        <div class="mb-3 text-start col">
+        <div class="mb-3 text-start col-2">
             <label for="rooms">Stanze</label>
             <input type="number" id="rooms" name="rooms"
                 class="form-control @error('rooms') is-invalid @elseif (old('rooms')) is-valid @enderror"
@@ -60,7 +129,7 @@
         </div>
 
         {{-- Beds --}}
-        <div class="mb-3 text-start col">
+        <div class="mb-3 text-start col-2">
             <label for="beds">Posti Letto</label>
             <input type="number" id="beds" name="beds"
                 class="form-control @error('beds') is-invalid @elseif (old('beds')) is-valid @enderror"
@@ -73,7 +142,7 @@
         </div>
 
         {{-- Bathrooms --}}
-        <div class="mb-3 text-start col">
+        <div class="mb-3 text-start col-2">
             <label for="bathrooms">Bagni</label>
             <input type="number" id="bathrooms" name="bathrooms"
                 class="form-control @error('bathrooms') is-invalid @elseif (old('bathrooms')) is-valid @enderror"
@@ -86,7 +155,7 @@
         </div>
 
         {{-- Mq --}}
-        <div class="mb-3 text-start col">
+        <div class="mb-3 text-start col-2">
             <label for="mq">Mq</label>
             <input type="number" id="mq" name="mq"
                 class="form-control @error('mq') is-invalid @elseif (old('mq')) is-valid @enderror"
@@ -99,7 +168,7 @@
         </div>
 
         {{-- Price --}}
-        <div class="mb-3 text-start col">
+        <div class="mb-3 text-start col-2">
             <label for="price">Prezzo a Notte</label>
             <input type="number" id="price" name="price"
                 class="form-control @error('price') is-invalid @elseif (old('price')) is-valid @enderror"
@@ -112,24 +181,26 @@
         </div>
     </div>
 
-    {{-- Visible switch --}}
-    <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" role="switch" id="is_visible" name="is_visible"
-            @if ($estate->is_visible) checked @endif>
-        <label class="form-check-label" for="is_visible">Visibile per tutti</label>
-    </div>
+    <div class="d-flex justify-content-between my-3">
+        {{-- Visible switch --}}
+        <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="is_visible" name="is_visible"
+                @if ($estate->is_visible) checked @endif>
+            <label class="form-check-label" for="is_visible">Visibile per tutti</label>
+        </div>
 
-    {{-- Dynamic Services Checkboxes --}}
-    <div class="d-flex justify-content-start my-5">
-        @foreach ($services as $service)
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" @if (in_array($service->id, old('services', $estate_service_ids ?? []))) checked @endif
-                    id="tech-{{ $service->id }}" value="{{ $service->id }}" name="services[]">
-                <label class="form-check-label" for="tech-{{ $service->id }}">{{ $service->label }}</label>
-            </div>
-        @endforeach
+        {{-- Dynamic Services Checkboxes --}}
+        <div class="d-flex justify-content-start">
+            @foreach ($services as $service)
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" @if (in_array($service->id, old('services', $estate_service_ids ?? []))) checked @endif
+                        id="tech-{{ $service->id }}" value="{{ $service->id }}" name="services[]">
+                    <label class="form-check-label" for="tech-{{ $service->id }}">{{ $service->label }}</label>
+                </div>
+            @endforeach
+        </div>
     </div>
-
+    {{-- !COVER --}}
     {{-- Image/cover input --}}
     {{-- <div class="col-10 text-start">
         <div class="mb-3">
@@ -148,7 +219,7 @@
     {{-- Multiple images --}}
     <div class="col-10 text-start">
         <div class="mb-3">
-            <label class="form-label" for="multiple_images">Foto multiple</label>
+            <label class="form-label" for="multiple_images">Immagini</label>
             <input type="file" multiple id="multiple_images" name="multiple_images[]"
                 class="form-control @error('multiple_images') is-invalid @elseif (old('multiple_images')) is-valid @enderror"
                 value="{{ old('multiple_images', $estate->images) }}">
