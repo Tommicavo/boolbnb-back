7273bd7c4d3c16f9540a7aa60a06f6d8459240c4
Tommicavo <tommicava@icloud.com>
2023-09-29 00:14:46 +0200

    start autocomplete api after 1s

--- resources/js/address-select-generator.js
+++ resources/js/address-select-generator.js
@@ -1,38 +1,44 @@
 const input = document.getElementById('address');
 const autocomplete = document.getElementById('autocomplete');
 
+let timeoutId = null;
+
 // Pick searched address input after 3 char
 input.addEventListener('input', function (e) {
-    const query = e.target.value;
-    if (query.length < 10) {
-        autocomplete.innerHTML = '';
-        return;
-    }
-
-    // Pick endpoint from backend
-    const endpoint = `/proxy/${query}`;
-
-    // AXIOS API Call
-    axios.get(endpoint)
-        .then(response => {
-            const results = response.data.results;
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+        const query = e.target.value;
+        if (query.length < 3) {
             autocomplete.innerHTML = '';
-
-            results.forEach(element => {
-                const div = document.createElement('div');
-                div.textContent = element.address.freeformAddress;
-                autocomplete.classList.remove('d-none');
-
-                div.addEventListener('click', function () {
-                    input.value = element.address.freeformAddress;
-                    autocomplete.innerHTML = '';
-                    autocomplete.classList.add('d-none');
-                    input.setAttribute("readonly", "readonly");
+            return;
+        }
+    
+        // Pick endpoint from backend
+        const endpoint = `/proxy/${query}`;
+    
+        // AXIOS API Call
+        axios.get(endpoint)
+            .then(response => {
+                console.log(endpoint);
+                const results = response.data.results;
+                autocomplete.innerHTML = '';
+    
+                results.forEach(element => {
+                    const div = document.createElement('div');
+                    div.textContent = element.address.freeformAddress;
+                    autocomplete.classList.remove('d-none');
+    
+                    div.addEventListener('click', function () {
+                        input.value = element.address.freeformAddress;
+                        autocomplete.innerHTML = '';
+                        autocomplete.classList.add('d-none');
+                        input.setAttribute("readonly", "readonly");
+                    });
+                    autocomplete.appendChild(div);
                 });
-                autocomplete.appendChild(div);
-            });
-
-        }).catch(err => {
-            console.error(err)
-        })
+    
+            }).catch(err => {
+                console.error(err)
+            })
+    }, 1000);
 });
