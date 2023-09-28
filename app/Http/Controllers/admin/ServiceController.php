4b9e2ceb2d46957eb5b1a87b3098c37c106da7e7
Tommicavo <tommicava@icloud.com>
2023-09-28 19:09:42 +0200

    moved visit e service controller in admin folder

--- app/Http/Controllers/admin/ServiceController.php
+++ app/Http/Controllers/admin/ServiceController.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
-use App\Models\Visit;
+use App\Http\Controllers\Controller;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
-class VisitController extends Controller
+class ServiceController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -34,7 +35,7 @@ class VisitController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Visit $visit)
+    public function show(Service $service)
     {
         //
     }
@@ -42,7 +43,7 @@ class VisitController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Visit $visit)
+    public function edit(Service $service)
     {
         //
     }
@@ -50,7 +51,7 @@ class VisitController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Visit $visit)
+    public function update(Request $request, Service $service)
     {
         //
     }
@@ -58,7 +59,7 @@ class VisitController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Visit $visit)
+    public function destroy(Service $service)
     {
         //
     }
