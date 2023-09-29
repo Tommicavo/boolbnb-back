66f7164ecbf4961461a82e45ffb986d042430a76
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-09-29 18:21:08 +0200

    services filter controller

--- app/Http/Controllers/Api/EstateController.php
+++ app/Http/Controllers/Api/EstateController.php
@@ -27,7 +27,34 @@ class EstateController extends Controller
 
     public function filter(Request $request)
     {
+        // $data = $request->all();
+        // $estates = Estate::where('beds', '>=', $data['minBed'])
+        //     ->where('rooms', '>=', $data['minRoom'])
+        //     ->where('services', $data[]);
+
+
+        // return response()->json($data);
+
+
+
+
         $data = $request->all();
-        return response()->json($data);
+
+        // Estrai i servizi selezionati dall'array di servizi
+        $selectedServices = array_keys(array_filter($data['services'] ?? []));
+
+        // Ottieni tutti gli Estates che soddisfano i criteri
+        $estates = Estate::where('beds', '>=', $data['minBed'] ?? 0)
+            ->where('rooms', '>=', $data['minRoom'] ?? 0)
+            ->whereHas('services', function ($query) use ($selectedServices) {
+                $query->whereIn('label', $selectedServices); // assumendo che 'name' sia la colonna con il nome del servizio
+            }, '=', count($selectedServices))
+            ->with('images') // includi le immagini nella risposta
+            ->get();
+
+        // Restituisci i risultati
+        return response()->json([
+            'results' => $estates
+        ]);
     }
 }
