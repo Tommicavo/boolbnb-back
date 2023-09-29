2bb3b17e930506c65d79540e78df7663de6b11ad
marcocareddu <saldumaccu@tuta.io>
2023-09-29 13:06:16 +0200

    Fixed api images

--- app/Http/Controllers/Api/EstateController.php
+++ app/Http/Controllers/Api/EstateController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Estate;
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 class EstateController extends Controller
 {
     public function index()
     {
-        $estates = Estate::where('is_visible', true)->orderBy('updated_at', 'DESC')->get();
+        $images = Image::all();
+        $estates = Estate::where('is_visible', true)->orderBy('updated_at', 'DESC')->with('images')->get();
         return response()->json([
             'results' => $estates
         ]);
