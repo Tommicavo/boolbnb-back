bcb4eba1cff28a2ec8e3406da299993905e0e167
marcocareddu <saldumaccu@tuta.io>
2023-09-29 10:40:41 +0200

    Added API Estates

--- app/Http/Controllers/Api/EstateController.php
+++ app/Http/Controllers/Api/EstateController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Estate;
 use Illuminate\Http\Request;
 
 class EstateController extends Controller
 {
-    //
+    public function index()
+    {
+        $estates = Estate::where('is_visible', true)->orderBy('updated_at', 'DESC')->get();
+        return response()->json([
+            'results' => $estates
+        ]);
+    }
 }
