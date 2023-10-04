12188ae7ab4e651b589ec6ed1f0d4933a7f302a5
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-10-04 18:03:43 +0200

    sponsorship breakpoints

--- resources/views/admin/estates/index.blade.php
+++ resources/views/admin/estates/index.blade.php
@@ -27,7 +27,7 @@
                     <th scope="col" class="text-center d-none d-md-table-cell">Stanze</th>
                     <th scope="col" class="text-center d-none d-lg-table-cell">Letti</th>
                     <th scope="col" class="text-center d-none d-lg-table-cell">Bagni</th>
-                    <th scope="col" class="text-center">Superficie</th>
+                    <th scope="col" class="text-center d-none d-md-table-cell">Superficie</th>
                     <th scope="col" class="text-center">Prezzo</th>
                     <th scope="col" class="text-center">Visibile</th>
                     <th scope="col" class="text-center">Azioni</th>
@@ -41,7 +41,7 @@
                             <td class="text-center d-none d-md-table-cell">{{ $estate->rooms }}</td>
                             <td class="text-center d-none d-lg-table-cell">{{ $estate->beds }}</td>
                             <td class="text-center d-none d-lg-table-cell">{{ $estate->bathrooms }}</td>
-                            <td class="text-center">{{ $estate->mq }} m²</td>
+                            <td class="text-center d-none d-md-table-cell">{{ $estate->mq }} m²</td>
                             <td class="text-center">{{ $estate->price }} €</td>
                             <td class="text-center">
                                 @if ($estate->is_visible)
@@ -52,18 +52,18 @@
                             </td>
                             <td>
                                 <div class="d-flex justify-content-center">
-
-                                    <a class="btn btn-info ms-3"
-                                        href="{{ route('admin.estates.promo', $estate) }}">Promuovi</a>
-                                        
+                                    <a class="btn btn-dark me-2 me-md-3" href="{{ route('admin.estates.promo', $estate) }}">
+                                        <span class="d-none d-lg-inline">Promuovi</span>
+                                        <span class="d-lg-none"><i class="fa-solid fa-comment-dollar"></i></span>
+                                    </a>
                                     <a class="btn btn-info text-white" href="{{ route('admin.estates.show', $estate) }}">
-                                        <span class="d-none d-md-inline">Dettagli</span>
-                                        <span class="d-md-none"><i class="fa-solid fa-circle-info"></i></span>
+                                        <span class="d-none d-lg-inline">Dettagli</span>
+                                        <span class="d-lg-none"><i class="fa-solid fa-circle-info"></i></span>
                                     </a>
                                     <a class="btn btn-warning text-white mx-2 mx-md-3"
                                         href="{{ route('admin.estates.edit', $estate) }}">
-                                        <span class="d-none d-md-inline">Modifica</span>
-                                        <span class="d-md-none"><i class="fa-solid fa-wrench"></i></span>
+                                        <span class="d-none d-lg-inline">Modifica</span>
+                                        <span class="d-lg-none"><i class="fa-solid fa-wrench"></i></span>
                                     </a>
 
                                     <form action="{{ route('admin.estates.destroy', $estate) }}" method="POST"
@@ -72,8 +72,8 @@
                                         @method('DELETE')
                                         <button class="btn btn-danger" type="submit" data-bs-toggle="modal"
                                             data-bs-target="#myModal">
-                                            <span class="d-none d-md-inline">Elimina</span>
-                                            <span class="d-md-none"><i class="fa-solid fa-trash-can"></i></span>
+                                            <span class="d-none d-lg-inline">Elimina</span>
+                                            <span class="d-lg-none"><i class="fa-solid fa-trash-can"></i></span>
                                         </button>
                                     </form>
                                 </div>
--- resources/views/admin/estates/promo.blade.php
+++ resources/views/admin/estates/promo.blade.php
@@ -1,31 +1,31 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container h-50">
-        <div class=" d-flex row-cols-3 justify-content-center">
-            <div class="card col me-2">
-                <img src="http://[::1]:5173/public/Silver.png" class="card-img-top" alt="...">
+    <div class="container h-50 mt-4">
+        <div class="d-flex row-cols-3 row-cols-md-4 row-cols-xl-5 justify-content-center">
+            <div class="card col">
+                <img src="http://[::1]:5173/public/Silver.png" class="card-img-top" alt="Silver sponsorship">
                 <div class="card-body">
                     <h5 class="card-title">Silver</h5>
                     <p class="card-text">Metti in evidenza il tuo annuncio per 24 ore!</p>
-                    <a href="{{ route('admin.estates.payments') }}" class="btn btn-primary">
-                        <span>€2,99</span>
+                    <a href="{{ route('admin.estates.payments') }}" class="btn btn-dark">
+                        <span>€ 2,99</span>
                     </a>
                 </div>
             </div>
-            <div class="card col me-2">
-                <img src="http://[::1]:5173/public/Gold.png" class="card-img-top" alt="...">
+            <div class="card col mx-3">
+                <img src="http://[::1]:5173/public/Gold.png" class="card-img-top" alt="Gold sponsorship">
                 <div class="card-body">
                     <h5 class="card-title">Gold</h5>
                     <p class="card-text">Metti in evidenza il tuo annuncio per 72 ore!</p>
-                    <a href="{{ route('admin.estates.payments') }}" class="btn btn-primary">€5,99</a>
+                    <a href="{{ route('admin.estates.payments') }}" class="btn btn-dark">€ 5,99</a>
                 </div>
             </div>
             <div class="card col">
-                <img src="http://[::1]:5173/public/Platinum.png" class="card-img-top" alt="...">
+                <img src="http://[::1]:5173/public/Platinum.png" class="card-img-top" alt="Platinum sponsorship">
                 <div class="card-body">
                     <h5 class="card-title">Platinum</h5>
                     <p class="card-text">Metti in evidenza il tuo annuncio per 144 ore!</p>
-                    <a href="{{ route('admin.estates.payments') }}" class="btn btn-primary">€9,99</a>
+                    <a href="{{ route('admin.estates.payments') }}" class="btn btn-dark">€ 9,99</a>
                 </div>
             </div>
         </div>
