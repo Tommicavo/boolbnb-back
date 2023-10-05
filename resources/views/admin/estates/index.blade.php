cff712bf16d1c1768c8ee81681d632c35d187cba
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-10-05 17:33:15 +0200

    damn d-flex!

--- resources/views/admin/estates/index.blade.php
+++ resources/views/admin/estates/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('title', 'Annunci')
 @section('content')
-    <header class="d-flex justify-content-between align-items-center">
-        <h1 class="text-center py-3">Alloggi</h1>
+    <header class="d-flex justify-content-between align-items-center mt-3">
+        <h1 class="text-center">Alloggi</h1>
         <div class="headerRight">
             <a href="{{ route('admin.estates.create') }}" class="btn btn-success">
                 <span><i class="fa-regular fa-square-plus"></i></span>
@@ -19,16 +19,17 @@
             </a>
         </div>
     </header>
-    <div class="indexContent">
+    <div class="indexContent d-flex align-items-center justify-content-center">
         <table class="table mt-3 align-middle table-light">
             <thead>
                 <tr>
                     <th scope="col">Alloggio</th>
-                    <th scope="col" class="text-center d-none d-md-table-cell">Stanze</th>
+                    <th scope="col" class="text-center d-none d-lg-table-cell">Stanze</th>
                     <th scope="col" class="text-center d-none d-lg-table-cell">Letti</th>
                     <th scope="col" class="text-center d-none d-lg-table-cell">Bagni</th>
                     <th scope="col" class="text-center d-none d-md-table-cell">Superficie</th>
                     <th scope="col" class="text-center">Prezzo</th>
+                    <th scope="col" class="text-center">Sponsor</th>
                     <th scope="col" class="text-center">Visibile</th>
                     <th scope="col" class="text-center">Azioni</th>
                 </tr>
@@ -38,11 +39,18 @@
                     @if (Auth::id() === $estate->user_id)
                         <tr>
                             <th>{{ $estate->title }}</th>
-                            <td class="text-center d-none d-md-table-cell">{{ $estate->rooms }}</td>
+                            <td class="text-center d-none d-lg-table-cell">{{ $estate->rooms }}</td>
                             <td class="text-center d-none d-lg-table-cell">{{ $estate->beds }}</td>
                             <td class="text-center d-none d-lg-table-cell">{{ $estate->bathrooms }}</td>
                             <td class="text-center d-none d-md-table-cell">{{ $estate->mq }} m²</td>
                             <td class="text-center">{{ $estate->price }} €</td>
+                            <td class="text-center">
+                                @forelse($estate->sponsorships as $sponsorship)
+                                    {{ $sponsorship->name }}
+                                @empty
+                                    NO
+                                @endforelse
+                            </td>
                             <td class="text-center">
                                 @if ($estate->is_visible)
                                     <i class="text-success fa-solid fa-circle-check"></i>
@@ -52,20 +60,20 @@
                             </td>
                             <td>
                                 <div class="d-flex justify-content-center">
-                                    <a class="btn btn-dark me-2 me-md-3" href="{{ route('admin.estates.promo', $estate) }}">
+                                    <a class="btn btn-dark me-2 me-lg-3"
+                                        href="{{ route('admin.estates.promo', $estate) }}">
                                         <span class="d-none d-lg-inline">Promuovi</span>
                                         <span class="d-lg-none"><i class="fa-solid fa-comment-dollar"></i></span>
                                     </a>
                                     <a class="btn btn-info text-white" href="{{ route('admin.estates.show', $estate) }}">
-                                        <span class="d-none d-lg-inline">Dettagli</span>
-                                        <span class="d-lg-none"><i class="fa-solid fa-circle-info"></i></span>
+                                        <span class="d-none d-xl-inline">Dettagli</span>
+                                        <span class="d-xl-none"><i class="fa-solid fa-circle-info"></i></span>
                                     </a>
-                                    <a class="btn btn-warning text-white mx-2 mx-md-3"
+                                    <a class="btn btn-warning text-white mx-2 mx-lg-3"
                                         href="{{ route('admin.estates.edit', $estate) }}">
-                                        <span class="d-none d-lg-inline">Modifica</span>
-                                        <span class="d-lg-none"><i class="fa-solid fa-wrench"></i></span>
+                                        <span class="d-none d-xl-inline">Modifica</span>
+                                        <span class="d-xl-none"><i class="fa-solid fa-wrench"></i></span>
                                     </a>
-
                                     <form action="{{ route('admin.estates.destroy', $estate) }}" method="POST"
                                         class="deleteForm trashEstate" data-name="{{ $estate->title }}">
                                         @csrf
