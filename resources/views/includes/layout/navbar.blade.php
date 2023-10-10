b64f5642b2c43b6475a4309ffdd099d2045ffca5
cloudhuh <claudiu.huhulia@gmail.com>
2023-10-10 11:33:35 +0200

    minor fix logo position

--- resources/views/includes/layout/navbar.blade.php
+++ resources/views/includes/layout/navbar.blade.php
@@ -1,4 +1,4 @@
-<header id="header" class="d-flex align-items-center justify-content-between container-fluid">
+<header id="header" class="d-flex align-items-center justify-content-between container-fluid ps-4">
     <a href="http://localhost:5173/">
         <div class="flex-shrink-0 d-none d-sm-block">
             <img src="{{ url('/BOOLlogo.svg') }}" alt="Logo BoolBnB" class="logo">
